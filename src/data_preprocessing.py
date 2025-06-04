2f2f7680f348fec47ca645c1768047627d4f5645
ksganni <krishnasathvika.ganni05@gmail.com>
2025-06-04 12:09:28 -0400

    Modified data_preprocessing.py file

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -6,15 +6,20 @@ def load_data():
     file_path=os.path.join("data","WA_Fn-UseC_-Telco-Customer-Churn.csv")
     df=pd.read_csv(file_path)
 
+    # Dropping the customerID as it is not useful for prediction
+    if 'customerID' in df.columns:
+        df.drop("customerID",axis=1,inplace=True)
+
+    # Converting Totalcharges to numeric, coerce errors 
+    df['TotalCharges']=pd.to_numeric(df['TotalCharges'],errors='coerce')
+
+    # Filling the missing TotalCharges values with MonthlyCharges
+    df['TotalCharges']=df['TotalCharges'].fillna(df['MonthlyCharges']*df['tenure'])
+
+    # Converting SeniorCitizen from 0/1 to 'No'/'Yes'
+    df['SeniorCitizen']=df['TotalCharges'].apply(lambda x:'Yes' if x==1 else 'No')
+
     # Stripping spaces from column names
     df.columns=df.columns.str.strip()
-
-    # Converting Totalcharges to numeric, coerce errors and drop the missing
-    df['TotalCharges']=pd.to_numeric(df['TotalCharges'],errors='coerce')
-    df=df.dropna(subset=['TotalCharges'])
-
-    # Dropping customerID 
-    if 'customerID' in df.columns:
-        df=df.drop(columns=['customerID'])
-
+    
     return df
