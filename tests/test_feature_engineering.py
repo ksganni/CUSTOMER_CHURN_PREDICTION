18efe3fe9618c8fe29046d4a3c1bd9244b782eba
ksganni <krishnasathvika.ganni05@gmail.com>
2025-06-09 18:34:56 -0400

    Added More unit tests for feature_engineering.py

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -1,18 +1,63 @@
 from src.feature_engineering import encode_and_new
 import pandas as pd
 
-def test_encoding():
-    df=pd.DataFrame({
-        'gender':['Male','Female'],
-        'SeniorCitizen':['Yes','No'],
-        'Partner':['Yes','No'],
-        'Dependents':['No','Yes'],
-        'tenure':[1,20],
-        'MonthlyCharges':[30,70],
-        'TotalCharges':[30,1400],
-        'Churn':['Yes','No']
+def test_encoding_basic():
+    df = pd.DataFrame({
+        'gender': ['Male', 'Female'],
+        'SeniorCitizen': ['Yes', 'No'],
+        'Partner': ['Yes', 'No'],
+        'Dependents': ['No', 'Yes'],
+        'tenure': [1, 20],
+        'MonthlyCharges': [30, 70],
+        'TotalCharges': [30, 1400],
+        'Churn': ['Yes', 'No']
     })
 
-    df_encoded=encode_and_new(df)
+    df_encoded = encode_and_new(df)
+
+    # Basic checks
     assert "Churn" in df_encoded.columns
-    assert df_encoded["Churn"].isin([0,1]).all()
+    assert df_encoded["Churn"].isin([0, 1]).all()
+
+    # Checking new feature ChargesPerMonth exists and is correct
+    expected = df["TotalCharges"] / (df["tenure"] + 1)
+    expected.name = "ChargesPerMonth"  # Fix: set name
+    assert "ChargesPerMonth" in df_encoded.columns
+    pd.testing.assert_series_equal(df_encoded["ChargesPerMonth"], expected)
+
+
+def test_encoding_with_reference_columns():
+    df = pd.DataFrame({
+        'gender': ['Male'],
+        'SeniorCitizen': ['Yes'],
+        'Partner': ['No'],
+        'Dependents': ['No'],
+        'tenure': [0],
+        'MonthlyCharges': [50],
+        'TotalCharges': [0],
+        'Churn': ['No']
+    })
+
+    # Providing reference columns that include some columns not in df after encoding
+    reference_columns = [
+        'ChargesPerMonth',
+        'gender_Male',     # created by get_dummies
+        'SeniorCitizen_Yes',
+        'Partner_Yes',     # missing from df, should be added as 0
+        'Dependents_Yes',  # missing from df, should be added as 0
+        'Churn'
+    ]
+
+    df_encoded = encode_and_new(df, reference_columns=reference_columns)
+
+    # All reference columns should be present in the result
+    assert set(reference_columns) == set(df_encoded.columns)
+
+    # Missing columns (Partner_Yes, Dependents_Yes) should be filled with zeros
+    assert (df_encoded['Partner_Yes'] == 0).all()
+    assert (df_encoded['Dependents_Yes'] == 0).all()
+
+    # ChargesPerMonth calculation
+    expected_charges = df["TotalCharges"] / (df["tenure"] + 1)
+    expected_charges.name = "ChargesPerMonth"  # Fix: set name
+    pd.testing.assert_series_equal(df_encoded["ChargesPerMonth"], expected_charges)
