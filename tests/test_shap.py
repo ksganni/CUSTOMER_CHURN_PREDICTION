8d40b1db30970742740cb04d8ffda248ed6a01a5
ksganni <krishnasathvika.ganni05@gmail.com>
2025-06-10 23:01:28 -0400

    Added Unit tests for SHAP

---
+++ tests/test_shap.py
@@ -0,0 +1,21 @@
+from src.feature_engineering import encode_and_new
+from src.data_preprocessing import load_data
+import pickle
+import shap
+
+def test_shap_explanation_runs():
+    df = encode_and_new(load_data())
+    X = df.drop("Churn", axis=1)
+
+    with open("models/best_model.pkl", "rb") as f:
+        loaded = pickle.load(f)
+        if isinstance(loaded, tuple):
+            model = loaded[0] 
+        else:
+            model = loaded  
+
+    explainer = shap.TreeExplainer(model)
+
+    shap_values = explainer.shap_values(X.sample(1))
+
+    assert shap_values is not None
